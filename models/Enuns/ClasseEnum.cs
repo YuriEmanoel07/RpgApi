a208be1f23fa53305cff57ed3414c3c5c13f8c50
Etec Prof. Horácio Augusto da Silveira <matheusborgesribeiro8@gmail.com>
2026-08-26 22:19:37 -0300

    Configurando classes

---
+++ models/Enuns/ClasseEnum.cs
@@ -0,0 +1,11 @@
+namespace RpgApi.models.Enuns
+{
+    public enum ClasseEnum
+    {
+        Cavaleiro = 1,
+
+        Mago = 2,
+
+        Clerigo = 3
+    }
+}
---
+++ models/Personagem.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using RpgApi.models.Enuns;
+
+namespace RpgApi.models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = "";
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        public ClasseEnum Classe { get; set; }
+
+    }
+}
